018d4316e454a6ff609fdbc77419b970ed681372
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-09-20 06:21:26 +0900

    会員登録のbladeを返した

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>会員登録 | 投稿サイト</title>
+</head>
+<body>
+    <header>
+        <h1>投稿サイト</h1>
+    </header>
+    <main>
+        <p>新規会員登録する。</p>
+    </main>
+    <footer>
+        <span>&copy;Tomoyasu Sunagawa 2022.</span>
+    </footer>
+</body>
+</html>

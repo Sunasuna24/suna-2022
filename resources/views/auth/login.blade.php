04947187b2e022d8511281ddf3ffac6c8e12e04d
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-09-20 06:09:17 +0900

    /loginにアクセスしたらログインのbladeを返すようにした

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ログイン | 投稿アプリ</title>
+</head>
+<body>
+    <header>
+        <h1>投稿アプリ</h1>
+    </header>
+    <main>
+        <p>これはログイン画面です。</p>
+    </main>
+    <footer>
+        <span>&copy;Tomoyasu Sunagawa 2022.</span>
+    </footer>
+</body>
+</html>

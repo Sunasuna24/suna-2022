59e694f664ca2e4524d327d95ac3ea53fccbadef
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-09-20 06:16:13 +0900

    会員登録の時にauth.registerのbladeが帰ってくるのを確認するテストを書いた

---
+++ tests/Feature/RegisterControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegisterControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function 会員登録の画面が表示される()
+    {
+        $this->get(route('register'))->assertOk()->assertViewIs('auth.register');
+    }
+}

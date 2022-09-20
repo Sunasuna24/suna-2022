ede150404bbf28aa149eeb3c520040fb0bfd6aa2
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-09-20 11:49:21 +0900

    ダミーのテストファイルを追加した

---
+++ tests/Unit/RootTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+class RootTest extends TestCase
+{
+    /** @test */
+    function sample()
+    {
+        $this->assertTrue(true);
+    }
+}

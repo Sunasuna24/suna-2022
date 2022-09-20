1fd5159255eff75e5b6d7968eca17786c64133b2
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-09-20 12:00:19 +0900

    認証済みユーザーは/にアクセスできないようにした

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('top');
-})->name('top');
-
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::middleware('guest')->group(function() {
+    Route::get('/', function () {return view('top');})->name('top');
     Route::get('/register', [RegisterConteroller::class, 'index'])->name('register');
     Route::post('/register', [RegisterConteroller::class, 'register']);
 });
--- tests/Feature/RootTest.php
+++ tests/Feature/RootTest.php
@@ -2,15 +2,26 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class RootTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
     function rootにアクセスするとTOP画面が表示される()
     {
         $this->get(route('top'))->assertViewIs('top');
+    }
+
+    /** @test */
+    function 認証済みのユーザーはTOP画面にアクセスできない()
+    {
+        User::factory()->create();
+        $user = User::find(1);
+        $this->actingAs($user)->get(route('top'))->assertRedirect(route('home'));
     }
 }

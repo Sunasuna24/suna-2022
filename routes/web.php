a67aeb0258569c4eca87428c5cd8c46fd85712b1
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-09-20 12:17:05 +0900

    ログアウトする機能を作成した

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterConteroller;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::middleware('guest')->group(function() {
     Route::get('/', function () {return view('top');})->name('top');
---
+++ tests/Feature/LoginControllerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LoginControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function ログアウトボタンを押すとWebサイトからログアウトする()
+    {
+        User::factory()->create();
+        $user = User::first();
+
+        $this->actingAs($user)->post(route('logout'), [])->assertRedirect(route('top'));
+        $this->assertGuest();
+    }
+}

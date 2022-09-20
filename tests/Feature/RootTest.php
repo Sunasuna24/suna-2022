a882665026dd2a9abdb9095db63f0fd842eaeffe
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-09-20 11:45:27 +0900

    Unitディレクトリを作成した

---
+++ tests/Feature/RootTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RootTest extends TestCase
+{
+    /** @test */
+    function rootにアクセスするとTOP画面が表示される()
+    {
+        $this->get(route('top'))->assertViewIs('top');
+    }
+}
